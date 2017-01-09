3eea8ebeeeae484ecb5e33da057a509b26ba86ab
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-09 15:06:51 +0100

    add age ruby

--- numbers.rb
+++ numbers.rb
@@ -5,3 +5,15 @@
 
 nombres_secondes = 365*24*3600
 puts nombres_secondes
+
+#calcul ton age en secondes
+
+age_secondes = nombres_secondes*31
+puts age_secondes
+
+
+#calcul l'age en secondes avec date d'anniversaire
+today= Time.now.to_i
+anniversaire =Time.new(1985,2,16,8,0,0).to_i
+age_sec=today-anniversaire
+puts age_sec
