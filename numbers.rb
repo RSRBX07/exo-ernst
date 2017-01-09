d5872da258ea1ae6f750d0734f64a5ff710b2687
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-09 15:53:07 +0100

    add

--- numbers.rb
+++ numbers.rb
@@ -14,6 +14,11 @@ puts age_secondes
 
 #calcul l'age en secondes avec date d'anniversaire
 today= Time.now.to_i
-anniversaire =Time.new(1985,2,16,8,0,0).to_i    # ou Time.new "16/02/1985"
+anniversaire =Time.new(1985,2,16,8,0,0).to_i    
 age_sec=today-anniversaire
 puts age_sec
+
+#origine du Time
+today = Time.now.to_i
+year = today/nombres_secondes
+puts year
