156e6f25e50f7c9cbf6021faa979c03cf71e4182
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-10 16:29:32 +0100

    comptage

---
+++ song.rb
@@ -0,0 +1,30 @@
+=beginAllongeons la jambe
+Chanson de marche
+Ma poul' n'a plus qu' vingt-neuf poussins, 
+Ma poul' n'a plus qu' vingt-neuf poussins,
+Elle en avait trente.
+Allongeons la jambe,
+Allongeons la jambe, la jambe
+Car la route est longue.
+Allongeons la jambe, la jambe
+Car la route est longue.
+
+Ma poul' n'a plus qu' vingt-huit poussins, 
+Ma poul' n'a plus qu' vingt-huit poussins, 
+Elle en avait trente.
+Allongeons la jambe,
+Allongeons la jambe, la jambe
+Car la route est longue.
+Allongeons la jambe, la jambe
+Car la route est longue.
+
+Ma poul' n'a plus qu' vingt-sept poussins 
+Ma poul' n'a plus qu' vingt-sept poussins 
+Elle en avait trente.
+Allongeons la jambe,
+Allongeons la jambe, la jambe
+Car la route est longue.
+Allongeons la jambe, la jambe
+Car la route est longue.
+=end
+for n in (30..0)
