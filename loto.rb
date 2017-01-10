784af70b0e82c7c705a17ed4409fc90c6f2e7e3b
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-10 15:08:18 +0100

    add loto

--- loto.rb
+++ loto.rb
@@ -7,3 +7,9 @@ puts nombres.sort
 Array.new (49) 
 pickin_balls= array.new.Shuffle.Take(5).sort
 puts "le tirage est : #{pickin_balls}"
+
+#affichage de la gagnotte
+#entre 100 .500 kilo€
+
+cagnotte = rand (100..500)
+puts "la gagntotte est de :#{cagnotte}"
