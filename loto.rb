3d161bc878139834d20cfa6d71c1f93a80315467
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-10 15:08:58 +0100

    add loto

--- loto.rb
+++ loto.rb
@@ -10,6 +10,12 @@ puts "le tirage est : #{pickin_balls}"
 
 #affichage de la gagnotte
 #entre 100 .500 kilo€
+#la gagnotte est de 2millions d'euro'
+vendredi_13? = true
+if  vendredi_13?
+    cagnotte
+    cagnotte = rand (100..500)
 
-cagnotte = rand (100..500)
 puts "la gagntotte est de :#{cagnotte}"
+
+
