5a5a087e23e0d943d9aded2f8edd1b6bf89b77dd
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-10 17:06:33 +0100

    add loto

--- loto.rb
+++ loto.rb
@@ -4,8 +4,7 @@
 
 #puts nombres.sort
 
-Array.new (49) 
-pickin_balls= array.new.Shuffle.Take(5).sort
+Bag = (1..49) pickin_balls= Shuffle.Take(5).sort
 puts "le tirage est : #{pickin_balls}"
 
 #affichage de la gagnotte
