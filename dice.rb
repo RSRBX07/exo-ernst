372615490b795d010281975a9e6eb1ee222e9c92
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-11 17:50:34 +0100

    add method

--- dice.rb
+++ dice.rb
@@ -1,17 +1,36 @@
 #creer un tableau (array) [], array.new
-faces = [1,2,3,4,5,6]
-puts faces
+#faces = [1,2,3,4,5,6]
+#puts faces
 #hasard=  rand 1..6
 
 #puts "votre numero est le #{hasard}"
 
 #retourne la valeur d'un dés
 
-def roll()
+=begin def roll()
     hasard =  rand 1..6
 end
 
+def roll_cheated cheated_value
+    cheated_value
+end
 puts "votre face est le #{roll}"
+puts "votre face est le #{roll}  "
+=end
+#methode avec ou sans des cheated 
+
+
+def roll (cheated_value = nil)
+    # si cheated_value return cheated_value
+    if cheated_value
+         cheated_value
+    # sinon return de normal
+    else 
+         rand 1..6
+    end
+end
+puts "votre lancée est #{roll 5}"
+
 
 
 
