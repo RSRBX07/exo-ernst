9c741557af95d3caf139ff4c10bbc6000f435b31
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-12 14:14:26 +0100

    add digits

---
+++ french_number.rb
@@ -0,0 +1,23 @@
+#ecrire une methode 
+#-qui prend une instance de fixnum en argument
+#-retourne la chaine de caractere du nombre ecrit en toute lettre en francais
+
+puts "tape un chiffre de 0 à 9"
+keep = gets.chomp
+def fixnum
+     chiffres = {
+    0: zero     
+    1: un,
+    2: deux
+    3: trois
+    4: quatre
+    5: cinq
+    6: six
+    7: sept
+    8: huit
+     9: neuf 
+    }
+return chiffres if 
+
+
+end
