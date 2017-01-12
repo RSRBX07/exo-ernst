e681211892d48716cfa50527ec43d9972a353b38
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-12 11:48:31 +0100

    listecodepostaux

---
+++ codepostal.rb
@@ -0,0 +1,21 @@
+#creation
+
+codes_postaux={
+    "lille" => "59000",
+    
+    "roubaix"=>"59300"
+}
+
+#afficher
+puts "de quel ville voulez vous connaitre le code postal ?"
+
+#recupere la valeur 
+city= gets.chomp
+
+#comparaison de la valeur
+codepostal=  codes_postaux[city] 
+
+puts city , codepostal
+#puts "Le code postal de #{""} est #{""}"
+
+
--- hash.rb
+++ hash.rb
@@ -6,14 +6,23 @@ meta_hash = { "place" => "roubaix", "author" =>"ernst" }
 puts "made with love @ #{meta_hash["place"]} by #{meta_hash["author"]}"
 #autre methode
 
-mon_hash = { 
-    14=> "ilyesse"
-    9=> "safya"
-    20=> "vincent"
-    'place'=> "roubaix"
-}
+#mon_hash = { 
+ #   14=> "ilyesse"
+  #  9=> "safya"
+   # 20=> "vincent"
+    #'place'=> "roubaix"
+#}
 
-place = mon_hash['place']
-author= mon_hash['20']
+#place = mon_hash['place']
+#author= mon_hash['20']
 
-puts "made with love in @#{place} by #{author}"
+#puts "made with love in @#{place} by #{author}"
+
+#CREATION DE CODE POSTAL
+
+hash_identity= {}
+hash_identity={"ville" => "roubaix","lille","codepostal"=>"59300","59000"}
+
+puts "quel est votre code postal ?"
+zipcode= gets.chomp
+puts "votre code postal est @#{hash_identity["codepostal"], votre ville est #{hash_identity["ville""]}}
