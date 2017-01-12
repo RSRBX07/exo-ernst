76df9f68bd3facd454928e6e3280359e8e4600d8
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-12 11:01:31 +0100

    autre methode

--- hash.rb
+++ hash.rb
@@ -2,5 +2,18 @@
 meta_hash= {}
 meta_hash = { "place" => "roubaix", "author" =>"ernst" }
 
-
+#affcher
 puts "made with love @ #{meta_hash["place"]} by #{meta_hash["author"]}"
+#autre methode
+
+mon_hash = { 
+    14=> "ilyesse"
+    9=> "safya"
+    20=> "vincent"
+    'place'=> "roubaix"
+}
+
+place = mon_hash['place']
+author= mon_hash['20']
+
+puts "made with love in @#{place} by #{author}"
