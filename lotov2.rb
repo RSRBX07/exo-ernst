fc2904940ece4372902e5261d19f1312bbfff490
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-16 16:17:25 +0100

    add empty

--- lotov2.rb
+++ lotov2.rb
@@ -1,23 +1,79 @@
-#affiche le tirage de 5 nombres entre 1 et 45
+require 'date'
+class Loto
 
-#nombres= [rand(1..45),rand(1..45),rand(1..45),rand(1..45),rand(1..45)]
+  def self.get_grid
+    grid = []
+    5.times do
+      input = gets.to_i
+      grid << input
+    end
+    grid
+  end
 
-#puts nombres.sort
+  def self.get_flash
+    (1..45).to_a.shuffle.take 5
+  end
 
-Bag = (1..49).to_a
-pickin_balls= bag.shuffle.take(5).sort
-puts "le tirage est : #{pickin_balls}"
+  def has_winner?
+    #comprer tous les bulletins valides avec la grille gagnante
+    sorted_draw = draw.sort
+    @saved_grids.each do |grid|
+      sorted_grid = grid.sort
+      return true if sorted_grid == sorted_draw
+    end
+    return false
+  end
 
-#affichage de la gagnotte
-entre 100 .500 kilo€
-la gagnotte est de 2millions d'euro'
-vendredi_13? = true
-if  vendredi_13?
-   cagnotte
-  cagnotte = rand (100..500)
+  # enregistre une grille
+  # pour le loto courant
+  def validate_grid grid
+  #verifier qu'un tirage n'as pas encore eu lieu 
+  if @picked_balls.to_a.empty?
+    end
 
-puts "la gagntotte est de :#{cagnotte}"
-#afficher si gagner ou perdu
+    # @saved_grids ||= []
 
-puts "votre jeu est  #{choix}"
+    @saved_grids = @saved_grids || []
+    @saved_grids.push grid
+  end 
+  # demander une grille de jeu
 
+  # affichage du montant de la cagnote
+  # entre 100 et 500.000 Euros
+  # le vendredi 13, la cagnote est de 2 millions
+  def vendredi_13?
+    Date.today.day == 13 && Date.today.friday?
+  end
+
+  
+
+  def prize
+    cagnote = if vendredi_13?
+        2_000_000
+      else
+        100_000
+      end
+    puts "Le montant de la cagnote du jour est de #{cagnote}"
+    cagnote
+  end
+
+  def draw
+    available_balls = (1..45).to_a
+    # shuffle balls and take 5
+    # @picked_balls ||= available_balls.shuffle.take(5)
+    @picked_balls = @picked_balls || available_balls.shuffle.take 5
+
+    puts "Le tirage du jour est : #{@picked_balls.sort}" 
+    @picked_balls
+  end
+
+  def check_grid grid
+    # afficher si gagne ou perdu
+    if grid.sort == draw.sort
+      puts "You win !"
+    else
+      puts "You loose !"
+    end
+  end
+  
+end
