de13f33f5cdcfd4e574948907e75b7371613296b
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-16 11:40:32 +0100

    new test loto.rb

---
+++ lotov2.rb
@@ -0,0 +1,23 @@
+#affiche le tirage de 5 nombres entre 1 et 45
+
+#nombres= [rand(1..45),rand(1..45),rand(1..45),rand(1..45),rand(1..45)]
+
+#puts nombres.sort
+
+Bag = (1..49).to_a
+pickin_balls= bag.shuffle.take(5).sort
+puts "le tirage est : #{pickin_balls}"
+
+#affichage de la gagnotte
+entre 100 .500 kilo€
+la gagnotte est de 2millions d'euro'
+vendredi_13? = true
+if  vendredi_13?
+   cagnotte
+  cagnotte = rand (100..500)
+
+puts "la gagntotte est de :#{cagnotte}"
+#afficher si gagner ou perdu
+
+puts "votre jeu est  #{choix}"
+
