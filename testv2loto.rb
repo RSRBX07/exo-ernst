9ccec30c4cfe371ac59d67a4931ca234954eb85c
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-17 12:25:06 +0100

    add commentary

--- testv2loto.rb
+++ testv2loto.rb
@@ -1,7 +1,20 @@
-
+#loto 
+#test tirage 5 nombres de loto
 require './lotov2.rb'
 
-my_loto = Lotov2.new
+my_loto = Loto.new
+puts my_loto.methods.inspect
+puts "le tirage est #{loto.picked_balls}"
+#loto.validate_grid Loto.get_flash
+#loto.has_winner?
+#puts "le tirage est #{loto.picked_balls.inspect} "
+#loto.picked_balls = [42 , 42 , 42 , 42, 42]
+#puts "le tirage est #{loto.picked_balls.inspect}"
+
+#puts my_loto 
+
+
+
 
 
 
