43169aeda443c49a6f1979120c80c577fd65a2c3
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-17 16:17:06 +0100

    add expect

--- spec/loto_spec.rb
+++ spec/loto_spec.rb
@@ -3,11 +3,19 @@
 require './loto/loto.rb'
 
 RSpec.describe Loto do 
-it 'gets grids' do
-loto= Loto.new  
+xit 'gets grids' do 
 Loto.get_grid
 end
+
+it 'give a random flash grid'do
+expect(Loto.get_flash).not_to be_nil
+expect(Loto.get_flash).to_be_an Array
+expect(Loto.get_flash).to equal 5
+expect (Loto.get_flash).not_to equal Loto.get_flash
 end
+end
+
+
 
 
 
