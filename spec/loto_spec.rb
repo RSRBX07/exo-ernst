db55fe349a3bea6084c2b4552acaec4f865f8b24
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-17 15:30:11 +0100

    clean

--- spec/loto_spec.rb
+++ spec/loto_spec.rb
@@ -1,17 +1,8 @@
 #loto 
 #test tirage 5 nombres de loto
-require './loto.rb'
+require '.loto/loto.rb'
 
-my_loto = Loto.new
-puts my_loto.methods.inspect
-puts "le tirage est #{loto.picked_balls}"
-#loto.validate_grid Loto.get_flash
-#loto.has_winner?
-#puts "le tirage est #{loto.picked_balls.inspect} "
-#loto.picked_balls = [42 , 42 , 42 , 42, 42]
-#puts "le tirage est #{loto.picked_balls.inspect}"
 
-#puts my_loto 
 
 
 
