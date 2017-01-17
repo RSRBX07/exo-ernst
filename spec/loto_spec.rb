db44deeea33f4e9fc449d4c3969cb0f54eccb9ad
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-17 16:05:31 +0100

    add get

--- spec/loto_spec.rb
+++ spec/loto_spec.rb
@@ -1,6 +1,14 @@
 #loto 
 #test tirage 5 nombres de loto
-require '.loto/loto.rb'
+require './loto/loto.rb'
+
+RSpec.describe Loto do 
+it 'gets grids' do
+loto= Loto.new  
+Loto.get_grid
+end
+end
+
 
 
 
