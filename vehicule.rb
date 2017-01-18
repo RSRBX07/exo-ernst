22c9bca64a1c1f9bf61114c7d261c79604bc0a01
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-18 15:13:46 +0100

    add super

--- vehicule.rb
+++ vehicule.rb
@@ -2,14 +2,25 @@
 class Vehicule
   #attribut de position
   attr_reader :position
+  attr_reader :out_of_order
   #initialise la methode
   def initialize
-    @position = roubaix 
+
+    @position = :roubaix
+    @out_of_order = false
   end
 
+#methode d'instance move
   def move destination
     @position = destination
-    puts" i'm moving"
+    puts" i'm moving" #afficher je bouge 
   end
 
+  
+
 end
+# #instancier un nouveau vehicule
+# puts (citroen = Vehicule.new).inspect
+# #dire à la citroen de bouger 
+# citroen.position
+# citroen.move.
