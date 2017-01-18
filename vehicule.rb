8a5d9bb60806fcf13cc32305faa8574eff977704
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-18 12:28:27 +0100

    add destination

--- vehicule.rb
+++ vehicule.rb
@@ -1,13 +1,15 @@
-class vehicule
+#creation de la classe vehicule
+class Vehicule
+  #attribut de position
   attr_reader :position
-  
+  #initialise la methode
   def initialize
     @position = roubaix 
   end
-  
-  def move
+
+  def move destination
+    @position = destination
     puts" i'm moving"
   end
 
-end
 end
