0014a483010ecc4f18d10ccb7d089fc76abb7668
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-18 13:44:58 +0100

    add draw

--- loto/loto.rb
+++ loto/loto.rb
@@ -50,13 +50,13 @@ def has_winner?
   end 
 
 #definit le le tirage 
-  # def draw 
-  #   available_balls = (1..45).to_a
-  #   # shuffle balls and take 5
-  #   # @picked_balls ||= available_balls.shuffle.take(5)
-  #   @picked_balls = @picked_balls || available_balls.shuffle.take(5)
-  #   puts "Le tirage du jour est : #{@picked_balls.sort}" 
-  # end
+  def draw 
+    available_balls = (1..45).to_a
+   #shuffle balls and take 5
+   @picked_balls ||= available_balls.shuffle.take(5)
+     @picked_balls = @picked_balls || available_balls.shuffle.take(5)
+     puts "Le tirage du jour est : #{@picked_balls.sort}" 
+   end
 
 
   
