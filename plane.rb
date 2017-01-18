f603f413b87715a91c26677ca6bc54f3075bc7e3
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-18 13:44:40 +0100

    add plane.rb

---
+++ plane.rb
@@ -0,0 +1,18 @@
+#creation de la class
+class Plane
+  #attribut de flying
+  attr_reader :flying
+#initialiser la variable d'instance
+  def initialize
+    @flying = false
+  end
+  #methode decoller 
+  def takeoff
+    @flying = true
+  end
+#methode atterrir
+  def land
+  @flying = false
+  end
+  
+end
