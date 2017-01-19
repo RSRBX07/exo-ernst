43d731c2af9fec078fa3d947dad735dd7a3a9def
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-19 11:37:01 +0100

    add selcont

--- vehicule.rb
+++ vehicule.rb
@@ -1,8 +1,22 @@
+class counter 
+  
+end
 #creation de la classe vehicule
 class Vehicule
   #attribut de position
   attr_reader :position
   attr_reader :out_of_order
+
+#retourne the number of eixting objects(in_memory)
+#open irb
+#> Vehicule.count
+#=>0
+#>vehicule.new
+#>vehicule.count
+
+  def self.count
+    
+  end
   #initialise la methode
   def initialize
 
