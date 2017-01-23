989f6d3f99c632965cb9926faf6f3794d84a3b56
ernstanto <ernstantoinejoseph@gmail.com>
2017-01-23 16:05:48 +0100

    add remove players

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,15 @@
 class Game < ApplicationRecord
+  def add_player
+    @players += 1
+    
+  end
+  def remove_players
+  return @players if players < 0 ; @players -=1 
+  end
+  
+  private
+  def initialize
+    @players =0
+  end
+  
 end
